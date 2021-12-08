de1de4ee0b38e359bf06a58cebb6e37e67623fd0
X3MisNotAvailable <95325466+X3MisNotAvailable@users.noreply.github.com>
2021-12-09 00:48:17 +0800

    log in & home page

--- store/src/app/app-routing.module.ts
+++ store/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   {path : 'login-page', component: LoginPageComponent},
-  {path : 'home-page', component: HomePageComponent}
+  {path : 'home-page', component: HomePageComponent},
+  {path: 'register-page', component: RegisterPageComponent}
 ];
 
 @NgModule({
--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
